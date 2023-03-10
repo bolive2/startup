548e3e1b1d23bf2107b08caa9b1da6a250d15ded
Brannon Olive <bolive.228@gmail.com>
2023-03-09 19:43:06 -0700

    output variable

--- questoin.js
+++ questoin.js
@@ -1,5 +1,37 @@
-function createQ() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("question", nameEl.value);
-    window.location.href = "index.html";
-  }
+function storeAndDisplay() {
+    // Get the input value
+    var input = document.getElementById("input").value;
+    
+    // Store the input in local storage
+    localStorage.setItem("input", input);
+    
+    // Display the input in the output element
+    document.getElementById("output").innerHTML = "Input: " + input;
+}
+
+// Check if there is a stored input and display it
+if (localStorage.getItem("input")) {
+    document.getElementById("output").innerHTML = "Input: " + localStorage.getItem("input");
+}
+
+
+// function othername() {
+//     var input = document.getElementById("userInput").value;
+//     alert(input);
+// }
+
+// function createQ(){
+//     var q1 = document.getElementById("question").value;
+//     alert(input);
+// }
+
+
+// function createQ() {
+//     const nameEl = document.querySelector("#question");
+//     localStorage.setItem("question", nameEl.value);
+//     window.location.href = "index.html";
+//   }
+
+//   function getPlayerName() {
+//     return localStorage.getItem('question');
+//   }
