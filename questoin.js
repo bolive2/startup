54db616aeb044df20a423dd49b00363a4297aa8f
Brannon Olive <bolive.228@gmail.com>
2023-03-22 18:23:40 -0600

    revamp stage

--- questoin.js
+++ questoin.js
@@ -6,7 +6,7 @@ function storeAndDisplay() {
     localStorage.setItem("input", input);
     
     // Display the input in the output element
-    document.getElementById("output").innerHTML = "Input: " + input;
+    document.getElementById("output").innerHTML = "Question: " + input;
 }
 
 // Check if there is a stored input and display it
@@ -14,24 +14,3 @@ if (localStorage.getItem("input")) {
     document.getElementById("output").innerHTML = "Question: " + localStorage.getItem("input");
 }
 
-
-// function othername() {
-//     var input = document.getElementById("userInput").value;
-//     alert(input);
-// }
-
-// function createQ(){
-//     var q1 = document.getElementById("question").value;
-//     alert(input);
-// }
-
-
-// function createQ() {
-//     const nameEl = document.querySelector("#question");
-//     localStorage.setItem("question", nameEl.value);
-//     window.location.href = "index.html";
-//   }
-
-//   function getPlayerName() {
-//     return localStorage.getItem('question');
-//   }
